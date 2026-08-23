6a42f86007b1efc351d1db2bee997a776c19c9f3
Artromskiy <47901401+Artromskiy@users.noreply.github.com>
2026-08-23 21:01:51 +0300

    Migrate ChunkCapacitySweepBenchmarks.cs to dense iteration API

--- benchmarks/DeltaECS.Benchmarks/ChunkCapacitySweepBenchmarks.cs
+++ benchmarks/DeltaECS.Benchmarks/ChunkCapacitySweepBenchmarks.cs
@@ -14,23 +14,9 @@ public class DenseCapacitySweepBenchmarks
     private World _arrayWorld = null!;
     private Query _query;
     private ComponentId[] _components = Array.Empty<ComponentId>();
-    private AccessRequest _b0; private AccessRequest _b1; private AccessRequest _b2; private AccessRequest _b3;
-    private AccessRequest _b4; private AccessRequest _b5; private AccessRequest _b6; private AccessRequest _b7;
+    private WriteAccess _b0; private WriteAccess _b1; private WriteAccess _b2; private WriteAccess _b3;
+    private WriteAccess _b4; private WriteAccess _b5; private WriteAccess _b6; private WriteAccess _b7;
     private LegacyByteDenseReference _legacy = null!;
-
-    private struct DenseState
-    {
-        public AccessRequest B0;
-        public AccessRequest B1;
-        public AccessRequest B2;
-        public AccessRequest B3;
-        public AccessRequest B4;
-        public AccessRequest B5;
-        public AccessRequest B6;
-        public AccessRequest B7;
-    }
-
-    private static readonly QueryAction<DenseState> s_iterate = Iterate;
 
     private struct S0 { public float X; public float Y; }
     private struct S1 { public float X; public float Y; }
@@ -80,34 +66,28 @@ public class DenseCapacitySweepBenchmarks
     [Benchmark(Baseline = true)]
     public void DeltaECS_Array()
     {
-        var state = new DenseState
+        using var scope = _arrayWorld.OpenQuery(in _query);
+        var archetypes = scope.Archetypes;
+        while (archetypes.MoveNext())
         {
-            B0 = _b0,
-            B1 = _b1,
-            B2 = _b2,
-            B3 = _b3,
-            B4 = _b4,
-            B5 = _b5,
-            B6 = _b6,
-            B7 = _b7
-        };
-        _arrayWorld.Query(in _query, ref state, s_iterate);
-    }
-
-    private static void Iterate(ref DenseState state, ref QueryChunkCursor cursor)
-    {
-        var c0 = cursor.GetWrite(state.B0);
-        var c1 = cursor.GetWrite(state.B1);
-        var c2 = cursor.GetWrite(state.B2);
-        var c3 = cursor.GetWrite(state.B3);
-        var c4 = cursor.GetWrite(state.B4);
-        var c5 = cursor.GetWrite(state.B5);
-        var c6 = cursor.GetWrite(state.B6);
-        var c7 = cursor.GetWrite(state.B7);
-        while (cursor.MoveNext())
-        {
-            c0.Ref<S0>(cursor).X += c0.Ref<S0>(cursor).Y; c1.Ref<S1>(cursor).X += c1.Ref<S1>(cursor).Y; c2.Ref<S2>(cursor).X += c2.Ref<S2>(cursor).Y; c3.Ref<S3>(cursor).X += c3.Ref<S3>(cursor).Y;
-            c4.Ref<S4>(cursor).X += c4.Ref<S4>(cursor).Y; c5.Ref<S5>(cursor).X += c5.Ref<S5>(cursor).Y; c6.Ref<S6>(cursor).X += c6.Ref<S6>(cursor).Y; c7.Ref<S7>(cursor).X += c7.Ref<S7>(cursor).Y;
+            var chunks = archetypes.Current.Chunks;
+            while (chunks.MoveNext())
+            {
+                var slots = chunks.Current.Slots;
+                var c0 = slots.Get(_b0);
+                var c1 = slots.Get(_b1);
+                var c2 = slots.Get(_b2);
+                var c3 = slots.Get(_b3);
+                var c4 = slots.Get(_b4);
+                var c5 = slots.Get(_b5);
+                var c6 = slots.Get(_b6);
+                var c7 = slots.Get(_b7);
+                while (slots.MoveNext())
+                {
+                    c0.Ref<S0>(slots).X += c0.Ref<S0>(slots).Y; c1.Ref<S1>(slots).X += c1.Ref<S1>(slots).Y; c2.Ref<S2>(slots).X += c2.Ref<S2>(slots).Y; c3.Ref<S3>(slots).X += c3.Ref<S3>(slots).Y;
+                    c4.Ref<S4>(slots).X += c4.Ref<S4>(slots).Y; c5.Ref<S5>(slots).X += c5.Ref<S5>(slots).Y; c6.Ref<S6>(slots).X += c6.Ref<S6>(slots).Y; c7.Ref<S7>(slots).X += c7.Ref<S7>(slots).Y;
+                }
+            }
         }
     }
 
